7b96b3c30249e2be9d23e3400e5386a2cf39e627
Elie <ehj00@mail.aub.edu>
2025-02-24 22:14:45 +0200

    Added vercel.json for deployment

--- server.js
+++ server.js
@@ -455,6 +455,8 @@ app.post("/checkout", (req, res) => {
 
 
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`✅ Server running on port ${PORT}`);
 });
+
