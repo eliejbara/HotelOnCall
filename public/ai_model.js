ef89c2ba8b16f6215d912e7428d42430d35fe9c9
NourDwayri <nwd06@mail.aub.edu>
2025-03-17 22:36:37 +0200

    Update ai_model.js

--- public/ai_model.js
+++ public/ai_model.js
@@ -2,7 +2,7 @@ async function getResponse(userMessage) {
     const response = await fetch("https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill", {
         method: "POST",
         headers: { 
-            "Authorization": "Bearer YOUR_HUGGINGFACE_API_KEY",
+            "Authorization": "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",
             "Content-Type": "application/json"
         },
         body: JSON.stringify({ inputs: userMessage })
