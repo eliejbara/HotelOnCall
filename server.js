18bf29e9eef8e3b4089e6fe03aa9dd63d6203e49
NourDwayri <nwd06@mail.aub.edu>
2025-03-20 19:53:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1229,7 +1229,7 @@ app.get('/api/predict-demand', async (req, res) => {
     }
 
     // Construct the Railway API URL
-    const apiUrl = `https://web-production-f430d.up.railway.app/predict_demand?year=${year}&month=${month}&day_of_week=${day_of_week}&is_weekend=${is_weekend}&is_holiday_season=${is_holiday_season}&avg_lead_time=${avg_lead_time}&sum_previous_bookings=${sum_previous_bookings}&avg_adr=${avg_adr}&total_children=${total_children}`;
+    const apiUrl = `https://web-production-f430d.up.railway.app/predict-demand?year=${year}&month=${month}&day_of_week=${day_of_week}&is_weekend=${is_weekend}&is_holiday_season=${is_holiday_season}&avg_lead_time=${avg_lead_time}&sum_previous_bookings=${sum_previous_bookings}&avg_adr=${avg_adr}&total_children=${total_children}`;
 
     // Fetch data from the Railway API
     const response = await axios.get(apiUrl);
