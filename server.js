d41d9734bb2f6360af3ff20c6745e0eb4a94b4ad
ayanheili <ayanheili9@icloud.com>
2025-04-01 20:42:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const session = require('express-session');
-const PgSession = require("connect-pg-simple")(session);
+
 
 require('dotenv').config();
 
