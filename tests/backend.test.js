e5dba9b3ce1ed60a08065920528254c2c294eeb7
NourDwayri <nwd06@mail.aub.edu>
2025-04-14 14:56:39 +0300

    Update backend.test.js

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -10,6 +10,10 @@ jest.mock('pg', () => ({
     connect: jest.fn(),
   })),
 }));
+jest.mock('passport-google-oauth20', () => ({
+  Strategy: jest.fn(),
+}));
+
 
 describe("HotelOnCall Backend Routes", () => {
   let db;
