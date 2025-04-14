b87d1a25648d910537b964f4021d7755201241ef
NourDwayri <nwd06@mail.aub.edu>
2025-04-14 20:55:03 +0300

    Update passport.js

--- __mocks__/passport.js
+++ __mocks__/passport.js
@@ -1,8 +1,14 @@
-module.exports = {
-  initialize: () => (req, res, next) => next(),
-  session: () => (req, res, next) => next(),
-  authenticate: () => (req, res, next) => next(),
-  use: () => {},
-  serializeUser: () => {},
-  deserializeUser: () => {},
+const passport = {
+  authenticate: jest.fn((strategy, options) => {
+    return (req, res, next) => {
+      // Mock user data for different test scenarios
+      req.user = { email: 'testuser@example.com', userType: 'guest' }; // Default user is a guest
+      next();
+    };
+  }),
+  initialize: jest.fn(() => (req, res, next) => next()),
+  session: jest.fn(() => (req, res, next) => next())
 };
+
+// Export the mock
+module.exports = passport;
