8260b061de760787da962d99fe553fec5df9a8f4
NourDwayri <nwd06@mail.aub.edu>
2025-04-15 20:46:05 +0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,12 @@
+// db.js
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+});
+
+module.exports = {
+  pool,
+  client: pool, // alias for compatibility with the test
+};
